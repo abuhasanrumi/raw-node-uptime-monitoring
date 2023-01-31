446536a19dd70effb26a1e6f4dcdbe56b492118d
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-01-31 12:51:05 +0600

    created server and handled req to get paylod from POST method

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 
 // dependencies
 const http = require('http');
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 // app object - module scaffolding
 const app = {};
@@ -16,7 +18,7 @@ app.config = {
 
 // create server
 app.createServer = () => {
-    const server = http.createServer(handleReqRes);
+    const server = http.createServer(app.handleReqRes);
     server.listen(app.config.port, () => {
         console.log(`Listening to port ${app.config.port}`);
     });
@@ -24,6 +26,30 @@ app.createServer = () => {
 
 // handle request response
 app.handleReqRes = (req, res) => {
-    // response handle
-    res.end('Hello World');
+    // req handling
+
+    // get the url and parse it
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headersObject = req.headers;
+
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on('end', () => {
+        realData += decoder.end();
+        console.log(realData);
+        // response handle
+        res.end('Hello World');
+    });
 };
+
+// start server
+app.createServer();
