25d217550937ddf936d3749310b6231c91be6e3f
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-02-01 21:44:06 +0600

    put method implemented on checks, user can update check info now

--- handlers/routeHandler/checkHandler.js
+++ handlers/routeHandler/checkHandler.js
@@ -156,6 +156,78 @@ handler._check.get = (requestProperties, callback) => {
 }
 
 handler._check.put = (requestProperties, callback) => {
+    // check if the token id is valid
+    const id = typeof (requestProperties.body.id) === 'string' && requestProperties.body.id.trim().length === 20 ? requestProperties.body.id : false;
+
+    // validate inputs
+    const protocol = typeof (requestProperties.body.protocol) === 'string' && ['http', 'https'].indexOf(requestProperties.body.protocol) > -1 ? requestProperties.body.protocol : false;
+
+    const url = typeof (requestProperties.body.url) === 'string' && requestProperties.body.url.trim().length > 0 ? requestProperties.body.url : false;
+
+    const method = typeof (requestProperties.body.method) === 'string' && ['GET', 'POST', 'PUT', 'DELETE'].indexOf(requestProperties.body.method) > -1 ? requestProperties.body.method : false;
+
+    const successCodes = typeof (requestProperties.body.successCodes) === 'object' && requestProperties.body.successCodes instanceof Array ? requestProperties.body.successCodes : false;
+
+    const timeOutSeconds = typeof (requestProperties.body.timeOutSeconds) === 'number' && requestProperties.body.timeOutSeconds % 1 === 0 && requestProperties.body.timeOutSeconds >= 1 && requestProperties.body.timeOutSeconds <= 5 ? requestProperties.body.timeOutSeconds : false;
+
+    if (id) {
+        if (protocol || url || method || successCodes || timeOutSeconds) {
+            data.read('checks', id, (err1, checkData) => {
+                if (!err1 && checkData) {
+                    let checkObj = parseJSON(checkData)
+                    const token = typeof (requestProperties.headersObject.token) === 'string' ? requestProperties.headersObject.token : false;
+
+                    // validate token
+                    tokenHandler._token.verify(token, checkObj.userPhone, (tokenIsValid) => {
+                        if (tokenIsValid) {
+                            if (protocol) {
+                                checkObj.protocol = protocol
+                            }
+                            if (url) {
+                                checkObj.url = url
+                            }
+                            if (method) {
+                                checkObj.method = method
+                            }
+                            if (successCodes) {
+                                checkObj.successCodes = successCodes
+                            }
+                            if (timeOutSeconds) {
+                                checkObj.timeOutSeconds = timeOutSeconds
+                            }
+
+                            // store the obj
+                            data.update('checks', id, checkObj, (err2) => {
+                                if (!err2) {
+                                    callback(200, checkObj)
+                                } else {
+                                    callback(500, {
+                                        error: 'Server side error',
+                                    });
+                                }
+                            })
+                        } else {
+                            callback(403, {
+                                error: 'Authentication Error',
+                            });
+                        }
+                    })
+                } else {
+                    callback(500, {
+                        error: 'Server side error',
+                    });
+                }
+            })
+        } else {
+            callback(400, {
+                error: 'You must provide at least one field to update',
+            });
+        }
+    } else {
+        callback(400, {
+            error: 'You have a problem in your request',
+        });
+    }
 
 }
 
