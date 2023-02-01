2a2ef087c88bb3fcdc03efd146c0d1d6828a0092
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-02-01 21:17:09 +0600

    put method implemented, authenticated users can lookup thier added checks

--- handlers/routeHandler/checkHandler.js
+++ handlers/routeHandler/checkHandler.js
@@ -121,7 +121,38 @@ handler._check.post = (requestProperties, callback) => {
 }
 
 handler._check.get = (requestProperties, callback) => {
+    // check if the token id is valid
+    const id = typeof (requestProperties.queryStringObject.id) === 'string' && requestProperties.queryStringObject.id.trim().length === 20 ? requestProperties.queryStringObject.id : false;
 
+    if (id) {
+        // lookup the check
+        data.read('checks', id, (err, checkData) => {
+            if (!err && checkData) {
+                // lookup token
+                const token = typeof (requestProperties.headersObject.token) === 'string' ? requestProperties.headersObject.token : false;
+
+                // verify token
+                tokenHandler._token.verify(token, parseJSON(checkData).userPhone, (tokenIsValid) => {
+                    if (tokenIsValid) {
+                        callback(200, parseJSON(checkData))
+                    } else {
+                        callback(403, {
+                            error: 'Authentication failure',
+                        });
+                    }
+                })
+            }
+            else {
+                callback(500, {
+                    error: 'Server side error',
+                });
+            }
+        });
+    } else {
+        callback(400, {
+            error: 'You have a problem in your request',
+        });
+    }
 }
 
 handler._check.put = (requestProperties, callback) => {
