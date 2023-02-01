c946723bf94eb5783aad13cb6ab5d9638c3e51a7
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-02-01 12:39:26 +0600

    deleting user functionality added with delete method

--- handlers/routeHandler/userHandler.js
+++ handlers/routeHandler/userHandler.js
@@ -138,7 +138,36 @@ handler._users.put = (requestProperties, callback) => {
     }
 }
 handler._users.delete = (requestProperties, callback) => {
+    // check if the phone no is valid
+    const phone = typeof (requestProperties.queryStringObject.phone) === 'string' && requestProperties.queryStringObject.phone.trim().length === 11 ? requestProperties.queryStringObject.phone : false;
 
+    if (phone) {
+        // lookup the user
+        data.read('users', phone, (err, userData) => {
+            if (!err && userData) {
+                data.delete('users', phone, (err) => {
+                    if (!err) {
+                        callback(200, {
+                            message: "User deleted successfully"
+                        })
+                    } else {
+                        callback(500, {
+                            error: "There was a server side error"
+                        })
+                    }
+                })
+            } else {
+                callback(500, {
+                    error: "There was a server side error"
+                })
+            }
+        })
+
+    } else {
+        callback(400, {
+            error: "There was a problem in your request"
+        })
+    }
 }
 
 module.exports = handler;
